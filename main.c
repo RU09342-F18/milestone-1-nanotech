2df421ab1c56fe9487e4d74bbeb6bb9967f7cc42
unknown <mclemoret0@students.rowan.edu>
2018-10-10 14:31:24 -0400

    Added test function

--- main.c
+++ main.c
@@ -9,17 +9,26 @@
 
 #include <msp430.h>
 
+#define Btn BIT3                 //Define "Btn" as bit 3.
+#define LED BIT0                   //Define "LED0" as bit 0.
+#define PnB (P1IN & Btn)        //Define "INP" for checking if there is an input on pin 1.3.
+typedef int bool;
+#define true 1
+#define false 0
+
 #define RedLED      BIT1;
 #define GreenLED    BIT2;
 #define BlueLED     BIT3;
 
 int ByteCount = 0;
+void TestCode();
 void TimerSetup(int rate);
 void UARTSetup();
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 	UARTSetup();
+	//TestCode();
 	return 0;
 }
 
@@ -41,8 +50,35 @@ void TimerSetup(int rate){//Subject to change
     CCTL0 = CCIE;
     TA0CTL = TASSEL_2 + MC_1 + ID_2; // SMCLK/4, Up
     TA0CCR0 = 125000 / rate; // 250000 / 10 = 25000, (10^6 [Hz] / 4) / (25000) = 10Hz
-}
+}/*P1.0 Will SWITCH
+void TestCode()
+{
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+    bool i = 0;                 //create a boolean called "i"
 
+    P1SEL &= (~LED & ~Btn);     //On P1 Select, set bits "0" and "3" to the value "0"
+
+    P1DIR |= LED;               //Set Port 1.0 as an output
+    P1OUT &= ~LED;              //Set the initial value of port 1.0 as "0"
+
+    P1DIR &= ~Btn;              //Set Port 1.3 as an input
+    P1REN |= Btn;               //Turn on the pull-up resistor for port 1.3
+    P1OUT |= Btn;               //Set the initial value of port 1.3 as "1"
+
+    while(1){                   //Start an infinite while loop.
+        if(!PnB && !i){         //If the button is pushed and the boolean is false
+                    P1OUT ^= LED;   //Flip the value of the LED
+                    i = 1;          //Flip the Value of the Boolean
+        }
+        else if(PnB && i){          //If the button is pushed and the boolean is true
+                   i = 0;           //Set the Boolean to be true
+       }
+
+    }
+
+
+}
+*/
 #pragma vector=USCI_A0_VECTOR               //Interrupt Vector definition
 __interupt void USCI_A0_ISR(void){          //Interrupt function deceleration
     switch(ByteCount){
