0e5cbac1bd60dbe655509bac5d47e02bae3ee7ec
Helen <panh1@students.rowan.edu>
2018-10-17 21:35:50 -0400

    changed some code for debug

--- main.c
+++ main.c
@@ -16,9 +16,9 @@ Test Code Definitions
 */
 
 /*
-#define Btn BIT3                            // Define "Btn" as bit 3.
-#define LED BIT0                            // Define "LED0" as bit 0.
-#define PnB (P1IN & Btn)                    // Define "INP" for checking if there is an input on pin 1.3.
+#define Btn BIT3                            	// Define "Btn" as bit 3.
+#define LED BIT0                            	// Define "LED0" as bit 0.
+#define PnB (P1IN & Btn)                    	// Define "INP" for checking if there is an input on pin 1.3.
 typedef int bool;
 #define true 1
 #define false 0
@@ -69,7 +69,7 @@ void UARTSetup()                           		// Code from Lab 0 example code
 	DCOCTL = CALDCO_1MHZ;
 	UCA0CTL1 |= UCSSEL_2;                   	// SMCLK
 	UCA0BR0 = 104;                          	// 1MHz 9600
-	UCA0BR1 = 0;                            	// 1MHz 9600
+	UCA0BR1 = 0;                            	// 1MHz 9600 (Ask About: line 71 and 72 have different values, 104 and 0)
 	UCA0MCTL = UCBRS0;                      	// Modulation UCBRSx = 1
 	UCA0CTL1 &= ~UCSWRST;                   	// Initialize USCI state machine
 	IE2 |= UCA0RXIE;                        	// Enable USCI_A0 RX interrupt
@@ -96,7 +96,6 @@ void LEDSetup()
 
 }
 
-
 /*
  * Setting Timer
  */
@@ -105,12 +104,12 @@ void TimerSetup(int rate)                  		// Subject to change
 {
 	CCTL0 = CCIE;
 	TA0CTL = TASSEL_2 + MC_1 + ID_0;        	// SMCLK divided by 1, Up
-	TA0CCR0  = 0x00FF;                  	// Sets CCR0 to 255
-	TA0CCTL1 = OUTMOD_7; 					// Reset or Set behavior
+	TA0CCR0  = 0x00FF;                  		// Sets CCR0 to 255
+	TA0CCTL1 = OUTMOD_7; 						// Reset or Set behavior
 
-	TA1CCR1  = 0x00FF;                      // Sets CCR0 to 255
-	TA1CCTL1 = OUTMOD_7;                    // Reset or Set behavior
-	TA1CCTL2 = OUTMOD_7;                    // Reset or Set behavior
+	TA1CCR0  = 0x00FF;                      	// Sets CCR0 to 255
+	TA1CCTL1 = OUTMOD_7;                    	// Reset or Set behavior
+	TA1CCTL2 = OUTMOD_7;                    	// Reset or Set behavior
 }
 
 
@@ -118,30 +117,30 @@ void TimerSetup(int rate)                  		// Subject to change
  * Setting UART Interrupt
  */
 
-//#pragma vector=USCI_A0_VECTOR           	// Interrupt Vector definition
-//__interupt void USCI_A0_ISR(void)         // Interrupt function deceleration
+//#pragma vector=USCI_A0_VECTOR           		// Interrupt Vector definition
+//__interupt void USCI_A0_ISR(void)         	// Interrupt function deceleration
 
 #pragma vector=USCIAB0RX_VECTOR
 __interrupt void USCI0RX_ISR(void)
 {
-    while (!(IFG2&UCA0TXIFG));            // USCI_A0 TX buffer ready?
+    while (!(IFG2&UCA0TXIFG));            		// USCI_A0 TX buffer ready?
 
           case 0:
-              NumberOfBytes = UCA0RXBUF;	// first byte received
+              NumberOfBytes = UCA0RXBUF;		// first byte received
               break;
           case 1:
-              TA0CCR1 = 255 - UCA0RXBUF;	// red LED value
+              TA0CCR1 = 255 - UCA0RXBUF;		// red LED value
               break;
           case 2:
-              TA1CCR1 = 255 -UCA0RXBUF;     // green LED value
+              TA1CCR1 = 255 -UCA0RXBUF;     	// green LED value
               break;
           case 3:
-              TA1CCR2 = 255 - UCA0RXBUF;      // blue LED value
-              UCA0TXBUF = NumberOfBytes - 3; // send new numBytes
+              TA1CCR2 = 255 - UCA0RXBUF;      	// blue LED value
+              UCA0TXBUF = NumberOfBytes - 3; 	// send new numBytes
               break;
           default:
               if(CurrentByte<NumberOfBytes){
-                  UCA0TXBUF = UCA0RXBUF;           // send remaining bytes
+                  UCA0TXBUF = UCA0RXBUF;		// send remaining bytes
               }
           }
           CurrentByte++;
@@ -154,28 +153,28 @@ __interrupt void USCI0RX_ISR(void)
 P1.0 Will SWITCH when you push the button, use this to test LEDs
 void TestCode()
 {
-	WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
-	bool i = 0;                             // Create a boolean called "i"
+	WDTCTL = WDTPW | WDTHOLD;               	// Stop watchdog timer
+	bool i = 0;                             	// Create a boolean called "i"
 
-	P1SEL &= (~LED & ~Btn);                 // On P1 Select, set bits "0" and "3" to the value "0"
+	P1SEL &= (~LED & ~Btn);                 	// On P1 Select, set bits "0" and "3" to the value "0"
 
-	P1DIR |= LED;                           // Set Port 1.0 as an output
-	P1OUT &= ~LED;                          // Set the initial value of port 1.0 as "0"
+	P1DIR |= LED;                           	// Set Port 1.0 as an output
+	P1OUT &= ~LED;                          	// Set the initial value of port 1.0 as "0"
 
-	P1DIR &= ~Btn;                          // Set Port 1.3 as an input
-	P1REN |= Btn;                           // Turn on the pull-up resistor for port 1.3
-	P1OUT |= Btn;                           // Set the initial value of port 1.3 as "1"
+	P1DIR &= ~Btn;                          	// Set Port 1.3 as an input
+	P1REN |= Btn;                           	// Turn on the pull-up resistor for port 1.3
+	P1OUT |= Btn;                           	// Set the initial value of port 1.3 as "1"
 
-	while(1)                                // Start an infinite while loop.
+	while(1)                                	// Start an infinite while loop.
     {
-		if(!PnB && !i)                      // If the button is pushed and the boolean is false
+		if(!PnB && !i)                      	// If the button is pushed and the boolean is false
         {
-					P1OUT ^= LED;           // Flip the value of the LED
-					i = 1;                  // Flip the Value of the Boolean
+					P1OUT ^= LED;           	// Flip the value of the LED
+					i = 1;                  	// Flip the Value of the Boolean
 		}
-		else if(PnB && i)                   // If the button is pushed and the boolean is true
+		else if(PnB && i)                   	// If the button is pushed and the boolean is true
         {
-				   i = 0;                   // Set the Boolean to be true
+				   i = 0;                   	// Set the Boolean to be true
 	    }
 
 	}
