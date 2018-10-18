b7850fd884206294eb890769a1cd508404d56009
McLemoreT <35839850+McLemoreT@users.noreply.github.com>
2018-10-18 18:24:44 -0400

    Reset CurrentByte when Finished sequence

--- main.c
+++ main.c
@@ -6,15 +6,10 @@
  *        Author: Tyler M. and Helen P.
  *         Board: MSP430G2553
  */
-
-
 #include <msp430g2553.h>
-
-
 /*
 Test Code Definitions
 */
-
 /*
 #define Btn BIT3                                // Define "Btn" as bit 3.
 #define LED BIT0                                // Define "LED0" as bit 0.
@@ -23,28 +18,21 @@ typedef int bool;
 #define true 1
 #define false 0
 */
-
 /*
  * Code Definition
  */
-
 #define RedLED      BIT6;
 #define GreenLED    BIT1;
 #define BlueLED     BIT4;
-
 // volatile int ByteCount = 0;
-
 void UARTSetup();
 void LEDSetup();
 void TimerSetup();
-
 int NumberOfBytes = 0;
 int CurrentByte = 0;
-
 /*
  * Main Function
  */
-
 int main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;                   // Stop watchdog timer
@@ -53,14 +41,11 @@ int main(void)
     TimerSetup();
     __bis_SR_register(GIE);
     while(1);
-
     return 0;
 }
-
 /*
  * Setting UART
  */
-
 void UARTSetup()                                // Code from Lab 0 example code
 {
     DCOCTL = 0;                               // Select lowest DCOx and MODx settings
@@ -75,12 +60,9 @@ void UARTSetup()                                // Code from Lab 0 example code
     UCA0CTL1 &= ~UCSWRST;                     // **Initialize USCI state machine**
     IE2 |= UCA0RXIE;                          // Enable USCI_A0 RX interrupt
 }
-
-
 /*
  * Setting LEDs
  */
-
 void LEDSetup()
 {
     P1DIR |= RedLED;                            // P1.6 to output
@@ -91,31 +73,24 @@ void LEDSetup()
     P2SEL2 |= ~GreenLED + ~BlueLED;
     P2DIR |= BlueLED;                           // P2.4 to output
     P2SEL |= BlueLED;                           // P2.4 to TA0.3
-
 }
-
 /*
  * Setting Timer
  */
-
 void TimerSetup()                       // Subject to change
 {
-
+    //CCTL0 = CCIE;
     TA0CTL = TASSEL_2 + MC_1 + ID_0;            // SMCLK divided by 1, Up
     TA0CCR0  = 255;                         // Sets CCR0 to 255
     TA0CCTL1 = OUTMOD_7;                        // Reset or Set behavior
-
     TA1CTL = TASSEL_2 + MC_1 + ID_0;            // SMCLK divided by 1, Up
     TA1CCR0  = 0x00FF;                          // Sets CCR0 to 255
     TA1CCTL1 = OUTMOD_7;                        // Reset or Set behavior
     TA1CCTL2 = OUTMOD_7;                        // Reset or Set behavior
 }
-
-
 /*
  * Setting UART Interrupt
  */
-
 #if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
 #pragma vector=USCIAB0RX_VECTOR
 __interrupt void USCI0RX_ISR(void)
@@ -126,9 +101,11 @@ void __attribute__ ((interrupt(USCIAB0RX_VECTOR))) USCI0RX_ISR (void)
 #endif
 {
     while (!(IFG2&UCA0TXIFG));                // USCI_A0 TX buffer ready?
+    //UCA0TXBUF = UCA0RXBUF;                    // TX -> RXed character
         switch(CurrentByte){
             case 0:
                 NumberOfBytes = UCA0RXBUF;      // first byte received
+                //UCA0TXBUF = UCA0RXBUF;      // Remove Later
                 break;
             case 1:
                 TA0CCR1 = 255 - UCA0RXBUF;      // red LED value
@@ -143,13 +120,12 @@ void __attribute__ ((interrupt(USCIAB0RX_VECTOR))) USCI0RX_ISR (void)
             default:
                 if(CurrentByte<NumberOfBytes){
                     UCA0TXBUF = UCA0RXBUF;      // send remaining bytes
+                }else{
+                    CurrentByte = 0;
                 }
             }
         CurrentByte++;
-
 }
-
-
 /* LED Test Code
 P1.0 Will SWITCH when you push the button, use this to test LEDs
 void TestCode()
